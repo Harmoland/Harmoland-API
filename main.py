284ae5f21a2d119a498d26e166607ea6fe6c799b
Redlnn <w731347477@gmail.com>
2023-05-28 17:43:33 +0800

    :sparkles: 使用loguru

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import asyncio
 import importlib
+import logging
 import pkgutil
+import sys
+from logging import StreamHandler
 from pathlib import Path
 
+from loguru import logger
 from uvicorn import Config
 
 from libs import app, db
 from libs.server import UvicornService
+from util import LoguruHandler
 
 # 加载api
 core_modules_path = Path('api')
@@ -15,6 +20,16 @@ for module in pkgutil.iter_modules([str(core_modules_path)]):
 
 
 service = UvicornService(Config(app, host='127.0.0.1', port=8080))
+loguru_handler = LoguruHandler()
+
+logging.basicConfig(handlers=[loguru_handler], level=0, force=True)
+for name in logging.root.manager.loggerDict:
+    _logger = logging.getLogger(name)
+    for handler in _logger.handlers:
+        if isinstance(handler, StreamHandler):
+            _logger.removeHandler(handler)
+logger.remove()
+logger.add(sys.stderr, level='INFO', enqueue=True)
 
 
 async def main():
@@ -30,4 +45,4 @@ try:
     loop.run_until_complete(main())
 except KeyboardInterrupt:
     loop.run_until_complete(service.stop())
-    print("Received exit, exiting")
+    logger.info("Received exit, exiting")
--- util/__init__.py
+++ util/__init__.py
@@ -1,11 +1,31 @@
+import logging
+import sys
 from contextlib import suppress
 from typing import Any, Dict, List, Union
 
 import orjson
 from fastapi import WebSocketDisconnect, status
 from fastapi.websockets import WebSocket
+from loguru import logger
 from pydantic import BaseModel as PyDanticBaseModel
 from websockets.exceptions import ConnectionClosedError, ConnectionClosedOK
+
+
+class LoguruHandler(logging.Handler):
+    def emit(self, record):
+        # Get corresponding Loguru level if it exists.
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message.
+        frame, depth = sys._getframe(6), 6
+        while frame and frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
 
 
 def orjson_dumps(v, *, default, **dumps_kwargs):
